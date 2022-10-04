6b02cae1ad7519549acc8d1ac6768ee5e734d77e
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2022-10-04 16:20:55 -0400

    Creating modal with backdrop

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal() {
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <div className="btn btn--alt">Cancel</div>
+      <div className="btn">Confirm</div>
+    </div>
+  );
+}
+
+export default Modal;
