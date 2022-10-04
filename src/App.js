043c9e068cbbe3d36f38e4ad7ed1d2f812be3113
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2022-10-04 15:12:08 -0400

    Delete button, card, and styling.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>
+        My Todos
+      </h1>
+      <div className='card'>
+        <h2>My Title</h2>
+        <div className='actions'>
+        <button className='btn'>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
